ad7b3de9eec19f85a6b3e628414918e9f2862466
Soupborsh <100529201+Soupborsh@users.noreply.github.com>
2026-05-20 19:48:31 +0500

    encoder working. TODO: test motor. Use driver for motor with encoder

--- firmware/xlnc_apex_robot/src/main.rs
+++ firmware/xlnc_apex_robot/src/main.rs
@@ -2,12 +2,18 @@
 #![no_main]
 
 extern crate embassy_rp as hal;
+use defmt::info;
 use embassy_executor::Spawner;
-use hal::block::ImageDef;
-use embassy_time::Timer;
+use hal::{
+    bind_interrupts,
+    block::ImageDef,
+    peripherals::PIO0,
+    pio::Pio,
+    pio_programs::rotary_encoder::{Direction, PioEncoder, PioEncoderProgram},
+};
 
 //Panic Handler
-use {panic_probe as _};
+use panic_probe as _;
 // Defmt Logging
 use defmt_rtt as _;
 
@@ -16,13 +22,27 @@ use defmt_rtt as _;
 #[used]
 pub static IMAGE_DEF: ImageDef = hal::block::ImageDef::secure_exe();
 
+bind_interrupts!(struct Irqs {
+    PIO0_IRQ_0 => hal::pio::InterruptHandler<PIO0>;
+});
+
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
     let p = hal::init(Default::default());
+    let Pio {
+        mut common, sm0, ..
+    } = Pio::new(p.PIO0, Irqs);
+    let prg = PioEncoderProgram::new(&mut common);
+    let mut encoder = PioEncoder::new(&mut common, sm0, p.PIN_16, p.PIN_17, &prg);
+    let mut c = 0;
 
-    loop{
-        Timer::after_millis(100).await;
-    }   
+    loop {
+        c += match encoder.read().await {
+            Direction::Clockwise => 1,
+            Direction::CounterClockwise => -1,
+        };
+        info!("{}", c);
+    }
 }
 
 // Program metadata for `picotool info`.
@@ -31,9 +51,7 @@ async fn main(_spawner: Spawner) {
 #[used]
 pub static PICOTOOL_ENTRIES: [embassy_rp::binary_info::EntryAddr; 4] = [
     embassy_rp::binary_info::rp_program_name!(c"xlnc_apex_robot"),
-    embassy_rp::binary_info::rp_program_description!(
-        c"your program description"
-    ),
+    embassy_rp::binary_info::rp_program_description!(c"your program description"),
     embassy_rp::binary_info::rp_cargo_version!(),
     embassy_rp::binary_info::rp_program_build_attribute!(),
 ];
