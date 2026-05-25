beb4c2297998d7693d41a1b71169efeef8939c8b
Soupborsh <100529201+Soupborsh@users.noreply.github.com>
2026-05-25 13:07:07 +0500

    Simple servo test

--- firmware/xlnc_apex_robot/src/main.rs
+++ firmware/xlnc_apex_robot/src/main.rs
@@ -3,11 +3,14 @@
 
 extern crate embassy_rp as hal;
 use embassy_executor::Spawner;
-use hal::block::ImageDef;
 use embassy_time::Timer;
+use hal::{
+    block::ImageDef,
+    pwm::{self, Pwm, SetDutyCycle as _},
+};
 
 //Panic Handler
-use {panic_probe as _};
+use panic_probe as _;
 // Defmt Logging
 use defmt_rtt as _;
 
@@ -19,10 +22,43 @@ pub static IMAGE_DEF: ImageDef = hal::block::ImageDef::secure_exe();
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
     let p = hal::init(Default::default());
+    const PWM_DIV_INT: u8 = 64;
+    const PWM_TOP: u16 = 46_874;
+    let mut servo_config: pwm::Config = Default::default();
+    servo_config.top = PWM_TOP;
+    servo_config.divider = PWM_DIV_INT.into();
 
-    loop{
-        Timer::after_millis(100).await;
-    }   
+    let mut servo = Pwm::new_output_a(p.PWM_SLICE3, p.PIN_22, servo_config);
+
+    loop {
+        // Move servo to 0° position (5 % duty cycle = 50/1000)
+        servo
+            .set_duty_cycle_fraction(50, 1000)
+            .expect("invalid min duty cycle");
+
+        Timer::after_millis(1500).await;
+
+        // 90° position (7.5 % duty cycle)
+        servo
+            .set_duty_cycle_fraction(75, 1000)
+            .expect("invalid half duty cycle");
+
+        Timer::after_millis(1500).await;
+
+        // 180° position (10% duty cycle)
+        servo
+            .set_duty_cycle_fraction(100, 1000)
+            .expect("invalid max duty cycle");
+
+        Timer::after_millis(1500).await;
+
+        // 90° position (7.5 % duty cycle)
+        servo
+            .set_duty_cycle_fraction(75, 1000)
+            .expect("invalid half duty cycle");
+
+        Timer::after_millis(1500).await;
+    }
 }
 
 // Program metadata for `picotool info`.
@@ -31,9 +67,7 @@ async fn main(_spawner: Spawner) {
 #[used]
 pub static PICOTOOL_ENTRIES: [embassy_rp::binary_info::EntryAddr; 4] = [
     embassy_rp::binary_info::rp_program_name!(c"xlnc_apex_robot"),
-    embassy_rp::binary_info::rp_program_description!(
-        c"your program description"
-    ),
+    embassy_rp::binary_info::rp_program_description!(c"your program description"),
     embassy_rp::binary_info::rp_cargo_version!(),
     embassy_rp::binary_info::rp_program_build_attribute!(),
 ];
