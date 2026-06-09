7e192fcca6201e5580638e9fdcea7a6b22d0efa4
Soupborsh <100529201+Soupborsh@users.noreply.github.com>
2026-06-10 03:36:19 +0500

    Use pixy2 WIP driver to get version

--- firmware/xlnc_apex_robot/src/main.rs
+++ firmware/xlnc_apex_robot/src/main.rs
@@ -4,15 +4,21 @@
 extern crate embassy_rp as hal;
 use defmt::dbg;
 use embassy_executor::Spawner;
-use embassy_time::Timer;
+use embassy_time::{Delay, Timer};
+use embedded_hal_bus::spi::ExclusiveDevice;
 use hal::{
-    bind_interrupts, block::ImageDef, gpio::{Level, Output}, peripherals::{DMA_CH0, DMA_CH1}, spi::{self, CsPin, Spi}
+    bind_interrupts,
+    block::ImageDef,
+    gpio::{Level, Output},
+    peripherals::{DMA_CH0, DMA_CH1},
+    spi::{self, Spi},
 };
 
 //Panic Handler
 use panic_probe as _;
 // Defmt Logging
 use defmt_rtt as _;
+use pixy2::Pixy2;
 
 /// Tell the Boot ROM about our application
 #[unsafe(link_section = ".start_block")]
@@ -29,23 +35,16 @@ async fn main(_spawner: Spawner) {
     spi_config.polarity = spi::Polarity::IdleHigh;
     spi_config.phase = spi::Phase::CaptureOnSecondTransition;
     dbg!("Ya amongaus!");
-    let mut cs_pin = Output::new(p.PIN_13, Level::High);
-    let mut spi_bus = Spi::new(
+    let cs_pin = Output::new(p.PIN_13, Level::High);
+    let spi_bus = Spi::new(
         p.SPI1, p.PIN_14, p.PIN_15, p.PIN_12, p.DMA_CH0, p.DMA_CH1, Irqs, spi_config,
     );
-    let tx: [u8; 4] = [
-        0xae, // first byte of no_checksum_sync (little endian -> least-significant byte first)
-        0xc1, // second byte of no_checksum_sync
-        0x0e, // this is the version request type
-        0x00, // data_length is 0
-    ];
     dbg!("Will do comunicaions");
-    // spi_bus.write(&tx).await.expect("Cannot write");
-    let mut rx = [0_u8; 35];
-    // spi_bus.read(&mut rx).await.expect("Cannot read");
-    cs_pin.set_low();
-    spi_bus.transfer(&mut rx, &tx).await.expect("Cannot transfer");
-    dbg!(rx);
+
+    let spi_dev =
+        ExclusiveDevice::new(spi_bus, cs_pin, Delay).expect("ExclusiveDevice creating failed");
+    let mut pixy2 = Pixy2::new(spi_dev);
+    dbg!(pixy2.get_version().await.unwrap());
 
     loop {
         Timer::after_millis(100).await;
