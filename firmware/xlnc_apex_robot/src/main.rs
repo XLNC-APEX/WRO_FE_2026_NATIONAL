334265ecdb894fde4d039e03c1a83183cbcd4a43
Soupborsh <100529201+Soupborsh@users.noreply.github.com>
2026-06-24 19:52:01 +0500

    [Code] WIP PurePursuit impl structure. No logic yet

--- firmware/xlnc_apex_robot/src/main.rs
+++ firmware/xlnc_apex_robot/src/main.rs
@@ -28,9 +28,21 @@ async fn main(spawner: Spawner) {
     info!("Intialized! Press btn2 to start. Then btn1 to reset.");
     devices.btn2.wait_for_low().await;
     spawner.spawn(btn_reset(devices.btn1, devices.watchdog).unwrap());
+
+    devices.otos.reset_tracking().await.unwrap();
+    devices.otos.calibrate_imu(255).await.unwrap();
+
     loop {
+        devices.btn2.wait_for_falling_edge().await;
+        let pos = devices.otos.get_pos().await.unwrap();
+        info!("{}", pos);
         Timer::after_millis(100).await;
     }
+    //Points to go through, measured irl by moving manually:
+    //Pose { x: 0.0, y: 0.0, h: -0.0022050973 }
+    //Pose { x: 0.054626465, y: -1.095581, h: -0.044773065 }
+    //Pose { x: 2.414856, y: -1.1758423, h: 1.5301459 }
+    //Pose { x: 2.3162842, y: -2.5476074, h: -0.08321846 }
 }
 
 // Program metadata for `picotool info`.
