56a29ab47364c1919eb59bbe9314a23abd697fc3
Soupborsh <100529201+Soupborsh@users.noreply.github.com>
2026-08-12 09:41:23 +0500

    SQUASH [code] pp::follower add tokio to run async tests. Start simulation, some refactoring.

--- firmware/xlnc_apex_robot/src/follower.rs
+++ firmware/xlnc_apex_robot/src/follower.rs
@@ -60,50 +60,42 @@ impl<T: Car, P: Path> PurePursuit<T, P> {
 
     fn get_target_point(&mut self, pos: Pose, vel: Vector2<f32>) -> Point2<f32> {
         // Note, vel does not need to be rotated by h, because we only need magnitude.
-        let p = Point2::<f32>::from(pos)
-            + Self::predict_pos(0.1, vel, self.config.l_drv, self.steer, pos.h);
-        let (tp, _) = self.path.next_closest_tp(p, 0.0);
+        let p = pos + predict_pos(0.1, vel, self.config.l_drv, self.steer, pos.h);
+        let (tp, _) = self.path.next_closest_tp(p.into(), 0.0);
         tp
         // TODO: move tp a bit along the path: tp = self.path.at_t(t+dt)
     }
+}
 
-    // TODO: test correctness
-    fn predict_pos(dt: f32, v: Vector2<f32>, l: f32, steer: f32, h: f32) -> Vector2<f32> {
-        let v = v.magnitude();
-        // Equation of rotation speed.
-        // I did it intuitively for rear drive bicycle model
-        let w = (v * sinf(2.0 * steer)) / (2.0 * l);
-        // Correction for fns discontinuity at w = 0
-        // Threshold may be tuned?
-        let (x, y) = if w.abs() > 1e-4 {
-            ((v * sinf(w * dt)) / w, (v * (1.0 - cosf(w * dt))) / w)
-        } else {
-            (v * dt, 0.0)
-        };
-        Rotation2::new(h) * Vector2::new(x, y)
-    }
+// TODO: test correctness
+fn predict_pos(dt: f32, v: Vector2<f32>, l: f32, steer: f32, h: f32) -> Pose {
+    let v = v.magnitude();
+    // Equation of rotation speed.
+    // I did it intuitively for rear drive bicycle model
+    let w = (v * sinf(2.0 * steer)) / (2.0 * l);
+    // Correction for fns discontinuity at w = 0
+    // Threshold may be tuned?
+    let (x, y) = if w.abs() > 1e-4 {
+        ((v * sinf(w * dt)) / w, (v * (1.0 - cosf(w * dt))) / w)
+    } else {
+        (v * dt, 0.0)
+    };
+    let mut pos: Pose = (Rotation2::new(h) * Vector2::new(x, y)).into();
+    pos.h = w * dt;
+    pos
 }
 
 #[cfg(test)]
 mod tests {
     use core::f32::consts::FRAC_PI_6;
 
-    use nalgebra::Vector2;
+    use nalgebra::{Point2, Vector2};
     use sparkfun_otos::Pose;
     extern crate std;
 
-    use crate::{follower::Car, follower::PurePursuit};
+    use crate::follower::{Car, PurePursuit, PurePursuitConfig, predict_pos};
+    use crate::path::LinesPath;
     use gnuplot::{AutoOption::Fix, AxesCommon, Figure};
-
-    struct MockCar;
-
-    impl Car for MockCar {
-        fn steer(&mut self, _pos: f32) {}
-        async fn reset(&mut self) {}
-        async fn get_pos_vel(&mut self) -> [sparkfun_otos::Pose; 2] {
-            [Pose::new(0.0, 0.0, 0.0); 2]
-        }
-    }
 
     #[test]
     fn plot_predict_pos() {
@@ -121,13 +113,7 @@ mod tests {
         ax.set_y_range(Fix(-0.5), Fix(0.5));
         ax.points([0.0], [0.0], &[]);
         for i in 0..N {
-            let pos = PurePursuit::<MockCar>::_predict_pos(
-                t,
-                Vector2::new(1.0, 0.0),
-                0.096,
-                -FRAC_PI_6,
-                0.0,
-            );
+            let pos = predict_pos(t, Vector2::new(1.0, 0.0), 0.096, -FRAC_PI_6, 0.0);
             // For FL(x forward, y left) coordinate system conversion:
             x[i] = -pos.y;
             y[i] = pos.x;
@@ -137,5 +123,67 @@ mod tests {
         ax.lines_points(x, y, &[]);
         fg.set_title("Path of car");
         fg.show().unwrap();
+    }
+
+    #[derive(Debug)]
+    struct SimCar {
+        pub pos: Pose,
+        pub vel: Pose,
+        pub steer: f32,
+    }
+
+    impl Car for SimCar {
+        fn steer(&mut self, pos: f32) {
+            self.steer = pos;
+        }
+        async fn reset(&mut self) {}
+        async fn get_pos_vel(&mut self) -> [sparkfun_otos::Pose; 2] {
+            [self.pos, self.vel]
+        }
+    }
+
+    impl SimCar {
+        fn new() -> Self {
+            SimCar {
+                pos: Pose::new(0.0, 0.0, 0.0),
+                vel: Pose::new(0.1, 0.0, 0.0),
+                steer: 0.0,
+            }
+        }
+    }
+
+    #[tokio::test]
+    async fn plot_sim_car() {
+        let car = SimCar::new();
+        let path_points = [
+            Point2::new(0.0, 0.0),
+            Point2::new(1.0, 0.0),
+            Point2::new(1.0, 1.0),
+            Point2::new(2.0, 1.0),
+        ];
+        let path = LinesPath::new(path_points);
+
+        let config = PurePursuitConfig {
+            kl: 1.0,
+            min_l: 0.1,
+            max_l: 0.5,
+            l_drv: 0.096,
+            max_steer: FRAC_PI_6,
+        };
+        let mut pp = PurePursuit::new(car, path, config);
+        const SIM_T: f32 = 2.0;
+        const DT: f32 = 0.01;
+        const N: usize = (SIM_T / DT) as usize;
+        for _ in 0..N {
+            pp.update().await;
+            pp.car.pos += predict_pos(
+                0.1,
+                pp.car.vel.into(),
+                pp.config.l_drv,
+                pp.steer,
+                pp.car.pos.h,
+            );
+            break;
+        }
     }
 }
